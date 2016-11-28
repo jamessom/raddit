f4cb9e5d89d65da4fcc2ed1c8c6044201c3506de
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2016-11-28 00:01:04 +0000

    Adicionando associacao entre Link e Usuario

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,5 @@
 class Link < ActiveRecord::Base
+  # Recebe o relacionamento criado em user.rb
+  # Informa que um link pertence a um usuário
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Adiciona relacionamento com o model/table links, adicionar associação
+  # Informa que o usuário tem muitos links
+  has_many :links
 end
